3bb7ac0f1294fb0da79586038940bf88da446fb4
adrichey <1534554+adrichey@users.noreply.github.com>
2024-06-28 10:57:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# vue-avatar-example
+Simple Vue avatar component example using the Vue 3 Options API.
+
+## To see it in action
+```
+npm install
+npm run dev
+```
+
+## Building and Previewing
+```
+npm run build
+npm run preview
+```
+
+## Running tests
+```
+npm run test
+```
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ui-avatar status="online" label="Guy Fieri" image="/assets/guy.jpg" />
+  </div>
+</template>
+
+<script>
+import UIAvatar from './components/UIAvatar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'ui-avatar': UIAvatar,
+  },
+}
+</script>
